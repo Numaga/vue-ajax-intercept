b08cfcd8ead39ea1a19ca9b36108551c0d701dbd
Vanderlei Silva <vanderleisilva@santos.sp.gov.br>
2017-06-21 15:27:44 -0300

    bugfix on finish event

--- src/ajax-intercept.vue
+++ src/ajax-intercept.vue
@@ -5,7 +5,6 @@
 export default {
     name: "AjaxIntercept",
     mounted() {
-
       var listener = { 
         tempOpen: XMLHttpRequest.prototype.open,
         tempSend: XMLHttpRequest.prototype.send,
@@ -14,11 +13,10 @@ export default {
       XMLHttpRequest.prototype.open = function(a='',b='') {
         listener.tempOpen.apply(this, arguments);
         eu.$emit('start', this);
-      }
 
-      XMLHttpRequest.prototype.send = function(a='',b='') {
-        listener.tempSend.apply(this, arguments);
-        eu.$emit('finish', this);
+        this.addEventListener('load', function() {
+            eu.$emit('finish', this);
+        });
       }
     }
 }
